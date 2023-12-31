0bd6fe203f233c2bb516f7b347c34cc8aa9f1bce
Eugene Debrah <debraheug@gmail.com>
2023-12-31 16:37:33 +0000

    Let's Setup

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <>
+      <div className='flex text-[56px] justify-center items-center font-bold'>
+        New Project
+      </div>
+    </>
+  );
+}
