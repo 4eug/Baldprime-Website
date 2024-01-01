1909d38594425fd2546770b76d1be0684a4ee590
Eugene Debrah <debraheug@gmail.com>
2024-01-01 08:44:59 +0000

    Update in Images

--- components/Service.jsx
+++ components/Service.jsx
@@ -4,15 +4,15 @@ import BlogCard from './cards/blogCard';
 export default function Service() {
   // Sample data for each blog card
   const blogData = [
-    { id: 1, title: 'Financial Audit', imageSrc: 'https://cdn.rareblocks.xyz/collection/celebration/images/blog/1/blog-post-1.jpg', content: 'In-depth financial statement audits to ensure accuracy and compliance with regulatory standards' },
-    { id: 2, title: 'Forensic Audit', imageSrc: 'https://cdn.rareblocks.xyz/collection/celebration/images/blog/1/blog-post-1.jpg', content: 'Investigative auditing to uncover fraud, resolve disputes, and ensure legal compliance.' },
-    { id: 3, title: 'Accounting Services', imageSrc: 'https://cdn.rareblocks.xyz/collection/celebration/images/blog/1/blog-post-1.jpg', content: 'Comprehensive accounting solutions, from bookkeeping to advanced financial reporting.' },
-    { id: 4, title: 'Taxation', imageSrc: 'https://cdn.rareblocks.xyz/collection/celebration/images/blog/1/blog-post-1.jpg', content: 'Expert tax services including planning, compliance, and advisory for individuals and corporations.' },
-    { id: 5, title: 'Financial Statements', imageSrc: 'https://cdn.rareblocks.xyz/collection/celebration/images/blog/1/blog-post-1.jpg', content: 'Precise preparation of financial reports for informed decision-making and compliance.' },
-    { id: 6, title: 'General Tax & Legal Services', imageSrc: 'https://cdn.rareblocks.xyz/collection/celebration/images/blog/1/blog-post-1.jpg', content: 'Efficient management of tax filings and GRA portal operations to ensure timely compliance.' },
-    { id: 7, title: 'Business Advisory', imageSrc: 'https://cdn.rareblocks.xyz/collection/celebration/images/blog/1/blog-post-1.jpg', content: 'Strategic consulting services to drive business growth and adapt to market changes.' },
-    { id: 8, title: 'General Tax & Legal Services', imageSrc: 'https://cdn.rareblocks.xyz/collection/celebration/images/blog/1/blog-post-1.jpg', content: 'Efficient management of tax filings and GRA portal operations to ensure timely compliance.' },
-    { id: 9, title: 'Global Transfer Pricing', imageSrc: 'https://cdn.rareblocks.xyz/collection/celebration/images/blog/1/blog-post-1.jpg', content: 'Strategic advice on cross-border transactions to comply with international tax laws.' },
+    { id: 1, title: 'Financial Audit', imageSrc: '/Financial Audit.png', content: 'In-depth financial statement audits to ensure accuracy and compliance with regulatory standards' },
+    { id: 2, title: 'Forensic Audit', imageSrc: '/Forensic Audit.png', content: 'Investigative auditing to uncover fraud, resolve disputes, and ensure legal compliance.' },
+    { id: 3, title: 'Accounting Services', imageSrc: '/Account Service.png', content: 'Comprehensive accounting solutions, from bookkeeping to advanced financial reporting.' },
+    { id: 4, title: 'Taxation', imageSrc: '/Taxation.png', content: 'Expert tax services including planning, compliance, and advisory for individuals and corporations.' },
+    { id: 5, title: 'Financial Statements', imageSrc: '/Financial Statement.png', content: 'Precise preparation of financial reports for informed decision-making and compliance.' },
+    { id: 6, title: 'General Tax & Legal Services', imageSrc: '/General Tax & Legal.png', content: 'Efficient management of tax filings and GRA portal operations to ensure timely compliance.' },
+    { id: 7, title: 'Business Advisory', imageSrc: '/Business advisory.png', content: 'Strategic consulting services to drive business growth and adapt to market changes.' },
+    { id: 8, title: 'General Tax & Legal Services', imageSrc: '/General Tax & Legal.png', content: 'Efficient management of tax filings and GRA portal operations to ensure timely compliance.' },
+    { id: 9, title: 'Global Transfer Pricing', imageSrc: '/Global Transfer.png', content: 'Strategic advice on cross-border transactions to comply with international tax laws.' },
   ];
 
   return (
