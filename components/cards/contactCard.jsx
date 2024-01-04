c406b5cb4a7629ce7340e80ec3ba2f012b8d5b7e
Eugene Debrah <debraheug@gmail.com>
2024-01-04 12:22:13 +0000

    adjustment

--- components/cards/contactCard.jsx
+++ components/cards/contactCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ContactCard({title, message, info}) {
   return (
-    <div className='border-l px-[32px]'>
+    <div className='border-l pl-[24px]'>
                 <h3 class="text-lg font-semibold text-black">{title}</h3>
                 <p class="mt-3 text-base text-[#606671]">{message}</p>
                 <p class="mt-4 text-[#266EFF] font-semibold text-[16px] leading-[24px]">
