a4ab885d76f29746724dfbb221dd3f21c1ccc5c5
Eugene Debrah <debraheug@gmail.com>
2024-01-04 12:22:01 +0000

    Update in Content Structure and rendering

--- components/cards/blogCard.jsx
+++ components/cards/blogCard.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react';
 
-const BlogCard = ({ title, imageSrc, content }) => {
+const BlogCard = ({ title, imageSrc, content1, content2, content3, content4, content5, showLearnMore  }) => {
   return (
     <div>
       <div className="block aspect-w-4 aspect-h-3">
@@ -14,9 +14,14 @@ const BlogCard = ({ title, imageSrc, content }) => {
       <p className="mt-6 text-xl font-semibold">
           {title}
       </p>
-      <p className="mt-2 text-gray-600">
-        {content}
-      </p>
+      <ul className="mt-2 pl-6 text-gray-600 list-disc">
+        <li>{content1}</li>
+        <li>{content2}</li>
+        {content3 && <li>{content3}</li>}
+        {content4 && <li>{content4}</li>}
+        {content5 && <li>{content5}</li>}
+      </ul>
+      {showLearnMore && <p className='pl-6 text-[#266EFF] text-sm font-semibold'>Learn more</p>}
     </div>
   );
 };
