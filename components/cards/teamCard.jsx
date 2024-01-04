0ccd5b0d74fdb3bde28cce471c3eb3d413e41894
Eugene Debrah <debraheug@gmail.com>
2024-01-04 12:22:20 +0000

    adjustment

--- components/cards/teamCard.jsx
+++ components/cards/teamCard.jsx
@@ -6,7 +6,7 @@ export default function TeamCard({name, title, imageSrc}) {
     <div>
     <div className="block aspect-w-4 aspect-h-3">
       <img
-        className="object-cover w-[240px] h-[214px] rounded-[12px]"
+        className="object-cover w-[144px] h-[144px] sm:w-full sm:h-full rounded-full"
         src={imageSrc}
         alt=""
       />
