688227ed6f92a1e9c0221033820f50e4c0f46944
Eugene Debrah <debraheug@gmail.com>
2024-01-07 20:26:36 +0000

    Update

--- components/cards/contactCard.jsx
+++ components/cards/contactCard.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function ContactCard({title, message, info}) {
   return (
     <div className='border-l pl-[24px]'>
-                <h3 class="text-lg font-semibold text-black">{title}</h3>
-                <p class="mt-3 text-base text-[#606671]">{message}</p>
-                <p class="mt-4 text-[#266EFF] font-semibold text-[16px] leading-[24px]">
+                <h3 className="text-lg font-semibold text-black">{title}</h3>
+                <p className="mt-3 text-base text-[#606671]">{message}</p>
+                <p className="mt-4 text-[#266EFF] font-semibold text-[16px] leading-[24px]">
                     {info}
                 </p>
     </div>
