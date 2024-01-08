4ad733aa96c3d6ded1597c02d7cf9a11a4a2fd2f
Eugene Debrah <debraheug@gmail.com>
2024-01-08 11:36:52 +0000

    Update

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable @next/next/no-img-element */
 import Meta from "../components/utils/meta";
@@ -77,7 +78,7 @@ export default function Home() {
   const teamMembers = [
     { id: 1, name: 'Stephen De-Graft Baidu', title: 'Managing Partner', imageSrc: '/Stephen De.png' },
     { id: 2, name: 'Oliver Kumordzie', title: 'Audit Partner', imageSrc: '/Oliver.png' },
-    { id: 3, name: 'Mathias Kwei', title: 'Audit Associate', imageSrc: '/Stephen De.png' },
+    { id: 3, name: 'Mathias Kwei', title: 'Audit Associate', imageSrc: '/Mathias.png' },
     { id: 4, name: 'Rosemary Dadzie', title: 'Head, Taxation & Assurance', imageSrc: '/Rosemary.png'},
     { id: 5, name: 'Lawrence Kokuvi Afodoanyi', title: 'Forensic Audit', imageSrc: '/Lawrence.png'},
   ];
@@ -103,7 +104,7 @@ export default function Home() {
       <nav className="bg-white fixed w-full z-20 top-0 mb-8">
       <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
         <a href="#"  className="flex items-center space-x-3 rtl:space-x-reverse">
-          <span className="self-center text-[30px] leading-[38px] font-extrabold whitespace-nowrap">BAAF</span>
+          <img src="/logo.png" className="w-[48px] h-[48px]"/>
         </a>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
         <a href="#" id="contact-link" className="font-semibold px-4 py-4 text-gray-900 border h-[40px] inline-flex items-center border-gray-300 rounded-[2px]">
