975d8f2a114e33e26ae7a564fa807b2a05a96654
Eugene Debrah <debraheug@gmail.com>
2024-01-23 22:31:41 +0000

    Footer image

--- pages/index.js
+++ pages/index.js
@@ -291,9 +291,9 @@ export default function Home() {
       <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
         <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-12 gap-y-12 gap-x-8 xl:gap-x-12">
           <div className="col-span-2 md:col-span-4 xl:pr-8">
-            <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
-              <span className="self-center text-[30px] leading-[38px] font-extrabold whitespace-nowrap">BAAF</span>
-            </a>
+          <a href="#"  className="flex items-center space-x-3 rtl:space-x-reverse">
+          <img src="/logo.png" className="w-[48px] h-[48px]"/>
+          </a>
             <div className="flex mt-8 space-x-[32px] text-[16px] font-semibold text-[#0E0F11]">
               <a href="https://www.linkedin.com/in/baidprime-auditing-and-accounting-firm-a8435b2a6/" target="_blank" rel="noopener noreferrer">linkedIn</a>
               <a href="https://www.instagram.com/baidprime/"  target="_blank" rel="noopener noreferrer">facebook</a>
